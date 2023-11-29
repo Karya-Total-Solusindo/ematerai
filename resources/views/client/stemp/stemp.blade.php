7f813ddbd72a6473fca301bc4750290e105466fd
Refyandra <irev@users.noreply.github.com>
2023-11-30 01:33:06 +0700

    update

--- resources/views/client/stemp/stemp.blade.php
+++ resources/views/client/stemp/stemp.blade.php
@@ -1,5 +1,6 @@
 <html>
  <head> 
+  <title>{{ env('APP_NAME') }} | STEMP DOCUMENT</title>
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
   <link href="{{ asset('./assets/css/nucleo-icons.css') }}" rel="stylesheet" />
