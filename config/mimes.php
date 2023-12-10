0d8f2987b52c4c3a2ac918d36c6d2ed07442d5f2
refyandra <refyandra@gmail.com>
2023-12-10 22:04:43 +0700

    mime zip

--- config/mimes.php
+++ config/mimes.php
@@ -5,8 +5,8 @@ return [
     /*
      * https://mimeapplication.net/ 
     */
-
-    'pdf'  => array('application/pdf', 'application/x-pdf', 'application/acrobat', 'applications/vnd.pdf', 'text/pdf', 'text/x-pdf'), 
+    'zip' => array('application/zip','application/x-zip-compressed','application/x-compressed','multipart/x-zip'),
+    'pdf' => array('application/pdf', 'application/x-pdf', 'application/acrobat', 'applications/vnd.pdf', 'text/pdf', 'text/x-pdf'), 
     'png' => array('image/png','application/x-png'),
     'doc'  => array('application/msword', 'application/vnd.ms-office'),
     'docx' => array('application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/zip'),
