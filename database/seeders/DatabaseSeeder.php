0ce698735c0628752721b0bf04663f6f693f2fb6
refyandra <refyandra@gmail.com>
2023-12-12 22:30:07 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
     {
         #TODO insert PermissionTableSeeder Data
         $this->call([
-            PermissionTableSeeder::class,
+           // PermissionTableSeeder::class,
         ]);
 
         $sadmin = User::create([
